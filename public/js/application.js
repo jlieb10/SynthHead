762dce7e5a7995561a712e00268a1f7b565ee0cd
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-30 11:42:10 -0400

    Updated Layout a Bit

--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,25 @@
 var videoInput = document.getElementById('inputVideo');
 var canvasInput = document.getElementById('inputCanvas');
-var htracker = new headtrackr.Tracker();
+var htracker = new headtrackr.Tracker({ui: false});
 htracker.init(videoInput, canvasInput);
 htracker.start();
-
 
 // create web audio api context
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-// create Oscillator and gain node
+// create oscillator and gain node
 var oscillator = audioCtx.createOscillator();
 var gainNode = audioCtx.createGain();
 
 // connect oscillator to gain node to speakers
-
 oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 
-// create initial theremin frequency and volumn values
-
+// find window size; set constants
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 
+// create initial theremin frequency and volumn values
 var maxFreq = 6000;
 var maxVol = 1;
 
@@ -36,39 +34,20 @@ oscillator.start();
 
 gainNode.gain.value = initialVol;
 
-// Mouse pointer coordinates
+// Estimated head coordinates
+var HeadX;
+var HeadY;
 
-var CurX;
-var CurY;
-
+// Change vals as head moves
 document.addEventListener("headtrackingEvent", function(e) {
     KeyFlag = false;
-    CurX = e.x*20;
-    CurY = -e.y*20;
-    oscillator.frequency.value = (CurX/WIDTH) * maxFreq;
-    gainNode.gain.value = (CurY/HEIGHT) * maxVol;
+    HeadX = e.x*20;
+    HeadY = -e.y*20;
+    oscillator.frequency.value = (HeadX/WIDTH) * maxFreq;
+    gainNode.gain.value = (HeadY/HEIGHT) * maxVol;
 }, false);
 
-
-// Get new mouse pointer coordinates when mouse is moved
-// then set new gain and pitch values
-
-// document.onmousemove = updatePage;
-
-// function updatePage(e) {
-//     KeyFlag = false;
-
-//     CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-//     CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-
-//     oscillator.frequency.value = (CurX/WIDTH) * maxFreq;
-//     gainNode.gain.value = (CurY/HEIGHT) * maxVol;
-
-//     //canvasDraw();
-// }
-
-// mute button
-
+// Mute button
 var mute = document.querySelector('.mute');
 
 mute.onclick = function() {
